f6051f93d3f90928dc69d93e9dfc6a2003314e5c
Zaeem104158 <zaeemhasan007@gmail.com>
2023-06-08 14:50:20 +0600

    Product data fetched using product data model class and showed in grid

--- lib/models/productdata_model_class.dart
+++ lib/models/productdata_model_class.dart
@@ -1,1 +1,35 @@
-class ProductDataModel {}
+import 'package:firebase_multi_vendor_project/utilits/common_constants.dart';
+
+class ProductDataModelClass {
+  String? productName;
+  String? productDescription;
+  String? mainCategory;
+  String? subCategory;
+  String? productPrice;
+  String? productInstock;
+  String? productDiscount;
+  List<String>? productImageFile;
+
+  ProductDataModelClass(
+      {this.productName,
+      this.productDescription,
+      this.mainCategory,
+      this.subCategory,
+      this.productPrice,
+      this.productInstock,
+      this.productDiscount,
+      this.productImageFile});
+
+  factory ProductDataModelClass.fromMap(Map<String, dynamic> map) {
+    return ProductDataModelClass(
+        productName: map[productCollectionFieldProductName],
+        productDescription: map[productCollectionFieldProductDescription],
+        mainCategory: map[productCollectionFieldMainCategory],
+        subCategory: map[productCollectionFieldSubCategory],
+        productPrice: map[productCollectionFieldProductPrice],
+        productInstock: map[productCollectionFieldProductInStock],
+        productDiscount: map[productCollectionFieldProductDiscount],
+        productImageFile:
+            map[productCollectionFieldProductImageFile].cast<String>());
+  }
+}
--- lib/views/home/gallery_widgets/men_gallery_widget.dart
+++ lib/views/home/gallery_widgets/men_gallery_widget.dart
@@ -1,40 +1,70 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_multi_vendor_project/components/design_component.dart';
 import 'package:firebase_multi_vendor_project/components/text_component.dart';
+import 'package:firebase_multi_vendor_project/models/productdata_model_class.dart';
+import 'package:firebase_multi_vendor_project/utilits/common_constants.dart';
 import 'package:firebase_multi_vendor_project/utilits/style.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
-class MenGalleryWidget extends StatelessWidget {
+class MenGalleryWidget extends StatefulWidget {
   const MenGalleryWidget({super.key});
 
   @override
+  State<MenGalleryWidget> createState() => _MenGalleryWidgetState();
+}
+
+class _MenGalleryWidgetState extends State<MenGalleryWidget> {
+  final Stream<QuerySnapshot> _productsStream =
+      FirebaseFirestore.instance.collection(productsDataDirectory).snapshots();
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomTextComponet(
-          textTitle: "Men",
-          isCenterText: false,
-          fontSize: appBarTitleTextSize,
-          isClickAble: false,
-          textPadding: EdgeInsets.zero,
-        ),
-        SizedBox(
-          height: customHeightWidth(context, height: true) * 0.68,
-          child: GridView.count(
-            crossAxisCount: 3,
-            crossAxisSpacing: 15,
-            mainAxisSpacing: 20,
-            children: List.generate(
-                5,
-                (index) => Container(
-                      color: cyanColor,
-                      child: Text("data"),
-                    )),
-          ),
-        ),
-      ],
+    return StreamBuilder<QuerySnapshot>(
+      stream: _productsStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(
+              color: cyanColor,
+            ),
+          );
+        }
+
+        return GridView.builder(
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 250,
+              childAspectRatio: 0.75,
+              crossAxisSpacing: 4,
+              mainAxisSpacing: 4),
+          itemCount: snapshot.data!.docs.length,
+          itemBuilder: (context, index) {
+            QueryDocumentSnapshot<Object?> productDataObject =
+                snapshot.data!.docs[index];
+            Map<String, dynamic> productData =
+                productDataObject.data() as Map<String, dynamic>;
+            final productDataModelClass =
+                ProductDataModelClass.fromMap(productData);
+            return Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(50)),
+                child: Column(
+                  children: [
+                    Text(productDataModelClass.subCategory!),
+                    Image.network(
+                      productDataModelClass.productImageFile![0],
+                      fit: BoxFit.contain,
+                    )
+                  ],
+                ));
+          },
+        );
+      },
     );
   }
 }
-//Category Screen Folder last er 1 ta video baki ase.
